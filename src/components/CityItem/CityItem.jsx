f6303d5b77bf52854243f040642dc2d7c57a14d2
Jose Copeti <jrcopeti@gmail.com>
2023-11-18 13:51:39 +0100

    get and set query string useSearchParams

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -9,10 +9,12 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+
+  const { cityName, emoji, date, id, position } = city;
+  console.log({position})
   return (
     <li >
-      <Link to={(`${id}`)} className={styles.cityItem}>
+      <Link to={(`${id}?lat=${position.lat}&lng=${position.lng}`)} className={styles.cityItem}>
 
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}> {cityName}</h3>
