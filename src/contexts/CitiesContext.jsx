b609656b46f2cbdd1c464ca4c6b5806bbdda7671
Jose Copeti <jrcopeti@gmail.com>
2023-11-23 19:46:44 +0100

    city view implemented

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -4,9 +4,10 @@ const CitiesContext = createContext();
 
 const BASE_URL = "http://localhost:8000";
 
-function CitiesProvider({children}){
+function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -25,17 +26,32 @@ function CitiesProvider({children}){
     fetchCities();
   }, []);
 
-  return (
-    <CitiesContext.Provider value={{
-      cities,
-      isLoading,
-    }}>
-      {children}
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      console.log(data);
+      setCurrentCity(data);
+    } catch {
+      console.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
+      {children}
     </CitiesContext.Provider>
-  )
+  );
 }
 
-
-
-export {CitiesProvider, CitiesContext};
+export { CitiesProvider, CitiesContext };
