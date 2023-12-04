b6f0f0e4068eaf3f8da071b42f5fb308df01818a
Jose Copeti <jrcopeti@gmail.com>
2023-12-04 23:50:59 +0100

    style responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,20 @@
-import { Outlet } from 'react-router-dom';
-import AppNav from '../AppNav/AppNav';
-import Footer from '../Footer/Footer';
-import Logo from '../Logo/Logo';
-import styles from './Sidebar.module.css';
+import { Outlet } from "react-router-dom";
+import AppNav from "../AppNav/AppNav";
+import Footer from "../Footer/Footer";
+import Logo from "../Logo/Logo";
+import styles from "./Sidebar.module.css";
 
 function Sidebar() {
-
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
-     <Outlet/>
+      <Outlet />
 
       <Footer />
-
-
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
